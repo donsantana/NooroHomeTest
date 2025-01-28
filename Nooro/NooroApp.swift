efe30043c5d730eb895dd9d545edf82411bcf185
Bocanasa, LLC <info@bocanasa.com>
2025-01-28 15:49:34 -0600

    Initial Commit

---
+++ Nooro/NooroApp.swift
@@ -0,0 +1,17 @@
+//
+//  NooroApp.swift
+//  Nooro
+//
+//  Created by Done Santana on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct NooroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NooroTests/NooroTests.swift
@@ -0,0 +1,17 @@
+//
+//  NooroTests.swift
+//  NooroTests
+//
+//  Created by Done Santana on 1/28/25.
+//
+
+import Testing
+@testable import Nooro
+
+struct NooroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
